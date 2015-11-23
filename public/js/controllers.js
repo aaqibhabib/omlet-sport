edbca2c8a03e0c8c10de649a49ce831e37e50d96
xunrongl <lixunrong@gmail.com>
2015-11-22 17:33:14 -0800

    add app flows

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,12 +1,27 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope,GameService) {
-  console.log("home");
-      $scope.games = GameService.getGames();
+    .controller('HomeCtrl', function($scope, GameService) {
+
+        GameService.getNbaGames().then(function(data) {
+            $scope.nba = data;
+        });
+
+        GameService.getNflGames().then(function(data) {
+            $scope.nfl = data;
+        });
+
     })
 
-.controller('CurrentGameCtrl', function($scope) {
-  console.log("game");
-})
+    .controller('CurrentGameCtrl', function($scope, GameService, $stateParams) {
+
+        GameService.getGameById($stateParams.id).then(function(data) {
+            if (data)
+            $scope.currentGame = data;
+
+        }, function(reason) {
+            console.log("can not get the score because" + reason);
+            $scope.currentGame = null;
+        })
+    })
 
 
--- public/js/services.js
+++ public/js/services.js
@@ -1,33 +1,62 @@
 angular.module('starter.services', [])
+    .factory('GameService', function ($http, $q) {
+        var expose = {};
 
-.factory('GameService', function() {
-  var expose = {};
+        expose.getNbaGames = function() {
 
-  expose.getGames = function() {
-    var games = [
-      {
-        home:"Cavaliers",
-        away:"76ers"
-      },
-      {
-        home:"Cavaliers",
-        away:"76ers"
-      },
-      {
-        home:"Cavaliers",
-        away:"76ers"
-      },
-      {
-        home:"Cavaliers",
-        away:"76ers"
-      },
-      {
-        home:"Cavaliers",
-        away:"76ers"
-      }
-    ]
-    return games;
-  }
+            var deferred = $q.defer();
 
-      return expose;
-});
+            $http.get("/data/nba.json").
+                success(function (data, status, headers, config) {
+                    if (data == null) {
+                        console.log("return empty list of vacation info");
+                    }
+                    deferred.resolve(data);
+                }).
+                error(function (data, status, headers, config) {
+                    console.log("could not get the vacation info, error status is " + status
+                    + " the response content is " + data);
+                    deferred.reject(data);
+                });
+            return deferred.promise;
+        };
+
+        expose.getNflGames = function() {
+
+            var deferred = $q.defer();
+
+            $http.get("/data/nfl.json").
+                success(function (data, status, headers, config) {
+                    if (data == null) {
+                        console.log("return empty list of vacation info");
+                    }
+                    deferred.resolve(data);
+                }).
+                error(function (data, status, headers, config) {
+                    console.log("could not get the vacation info, error status is " + status
+                    + " the response content is " + data);
+                    deferred.reject(data);
+                });
+            return deferred.promise;
+        }
+
+        expose.getGameById = function(id) {
+            var deferred = $q.defer();
+
+            $http.get("/data/game" + id + ".json").
+                success(function (data, status, headers, config) {
+                    if (data == null) {
+                        console.log("return empty list of vacation info");
+                    }
+                    deferred.resolve(data);
+                }).
+                error(function (data, status, headers, config) {
+                    console.log("could not get the vacation info, error status is " + status
+                    + " the response content is " + data);
+                    deferred.reject(data);
+                });
+            return deferred.promise;
+        }
+
+        return expose;
+    });
